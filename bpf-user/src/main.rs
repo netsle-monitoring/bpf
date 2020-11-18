659a8ab66969d155ed480852b957696fd9946da4
amit <gamesil456@gmail.com>
2020-11-18 22:35:04 +0200

    BPF Loader prototype

--- bpf-user/src/main.rs
+++ bpf-user/src/main.rs
@@ -1,4 +1,71 @@
-// This code will contain the custom user space BPF code loader
-fn main() {
-    // TODO:
+use futures::{future, stream::StreamExt};
+use redbpf::load::Loader;
+use std::path::Path;
+use redbpf::xdp::Flags;
+use redbpf::{Program::*};
+use tokio::signal;
+use std::io;
+use std::env;
+use std::str;
+
+/**
+ * The event map (the data structure we pass through the stream/maps)
+ */
+#[repr(C)]
+#[derive(Debug)]
+pub struct Event {
+    pub saddr: u32,
+    pub daddr: u32,
+    pub sport: u16,
+    pub dport: u16,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), io::Error>  {
+    let args: Vec<String> = env::args().collect(); // ARGV
+    
+    // check for args length
+    if args.len() != 3 {
+        eprintln!("usage: bpf_example_program [NETWORK_INTERFACE] [FILENAME]");
+        return Err(io::Error::new(io::ErrorKind::Other, "invalid arguments"));
+    }
+
+    let interface = args[1].clone();
+    let file = args[2].clone();
+
+    let mut loader = Loader::load_file(&Path::new(&file))
+    .expect("Error loading file...");
+
+    /**
+     * Load all of the XDP programs from the binary
+     */
+    for program in loader.module.programs.iter_mut() {
+        let name = program.name().to_string();
+        let _ret = match program {
+            XDP(prog) => {
+                println!("Attaching to {:?} interface: {:?}!!!", &name, &interface);
+                prog.attach_xdp(&interface, Flags::default()) // attach the program to the Kernel space
+            },
+            _ => Ok(())
+        };
+    }
+
+        // Listen to incoming map's data
+        tokio::spawn(async move {
+            let mut packet_num: u32 = 0;
+            while let Some((name, events)) = loader.events.next().await {
+                for event in events {
+                    packet_num += 1;
+                    println!("Packet #{:?}", packet_num);
+                }
+            }
+
+            // If the program doesn't have any maps and therefore doesn't fire any events, we still
+            // need to keep `loader` alive here so that BPF programs are not dropped. The future
+            // below will never complete, meaning that the programs will keep running until Ctrl-C
+            future::pending::<()>().await;
+        });
+
+
+    signal::ctrl_c().await
 }
