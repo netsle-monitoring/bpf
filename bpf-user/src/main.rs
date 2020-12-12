ec42f72892c755be9db2ee138e91d207499d8d74
amit <gamesil456@gmail.com>
2020-12-12 12:42:39 +0200

    Sending data to ES via logstash's tcp connection

--- bpf-user/src/main.rs
+++ bpf-user/src/main.rs
@@ -6,11 +6,13 @@ use std::path::Path;
 use std::time::Duration;
 use tokio::signal;
 use tokio::time::delay_for;
-
+use std::net::{TcpStream};
 use redbpf::{load::Loader, HashMap as BPFHashMap};
+use std::io::prelude::*;
 
 pub mod aggs;
 pub mod network_utils;
+pub mod elastic_mapping;
 
 #[tokio::main]
 async fn main() -> Result<(), io::Error> {
@@ -39,6 +41,8 @@ async fn main() -> Result<(), io::Error> {
         };
     }
 
+    let mut socket_connection = TcpStream::connect("localhost:5000").unwrap();
+
     // Listen to incoming map's data
     tokio::spawn(async move {
         let ips = BPFHashMap::<u32, aggs::IPAggs>::new(loader.map("ip_map").unwrap()).unwrap();
@@ -46,9 +50,11 @@ async fn main() -> Result<(), io::Error> {
             BPFHashMap::<u16, aggs::PortAggs>::new(loader.map("port_map").unwrap()).unwrap();
 
         loop {
-            delay_for(Duration::from_millis(1000)).await;
+            delay_for(Duration::from_millis(10000)).await;
             //format ips Hashmap into vec
             let ip_vec: Vec<(u32, aggs::IPAggs)> = ips.iter().collect();
+            let mut parsed_ips: Vec<elastic_mapping::EsReadyIpAggs> = Vec::new();
+
             println!("========Ips=======");
             for (k, v) in ip_vec.iter().rev() {
                 println!(
@@ -56,16 +62,38 @@ async fn main() -> Result<(), io::Error> {
                     network_utils::u32_to_ipv4(*k),
                     v.count
                 );
+                
+                let current_ip_agg = elastic_mapping::EsReadyIpAggs {
+                    ip: network_utils::u32_to_ipv4(*k).to_string(),
+                    count: v.count,
+                    usage: v.usage
+                };
+
+                parsed_ips.push(current_ip_agg);
                 ips.delete(*k);
             }
 
             //format port Hashmap into vec
             let port_vec: Vec<(u16, aggs::PortAggs)> = ports.iter().collect();
+            let mut parsed_ports: Vec<elastic_mapping::EsReadyPortAggs> = Vec::new();
             println!("========Ports=======");
             for (k, v) in port_vec.iter().rev() {
                 println!("{:?} - > count:{:?}", k, v.count);
+                let current_port_agg = elastic_mapping::EsReadyPortAggs {
+                    port: *k,
+                    count: v.count,
+                };
+
+                parsed_ports.push(current_port_agg);
                 ports.delete(*k);
             }
+
+            let data_iteration = elastic_mapping::BPFDataIteration {
+                ips: parsed_ips,
+                ports: parsed_ports,
+            };
+
+            socket_connection.write(&format!("{}\n", &serde_json::to_string(&data_iteration).unwrap()).as_bytes()).unwrap();
         }
     });
     signal::ctrl_c().await
